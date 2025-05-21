f0d6cdb61a987566f6dd30b05f476f12ad939b22
maximum0 <maximum.zero95@gmail.com>
2025-05-21 21:09:30 +0900

    chore: `ckt-core`, `ckt-admin` 모듈 생성
    - `ckt-core` : 핵심 비즈니스 도메인을 정의한 모듈
    - `ckt-admin` : 관리자 기능을 담당하는 실행 가능한 모듈

---
+++ ckt-admin/src/main/java/kernel360/ckt/admin/CktAdminApplication.java
@@ -0,0 +1,13 @@
+package kernel360.ckt.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CktAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CktAdminApplication.class, args);
+    }
+
+}
