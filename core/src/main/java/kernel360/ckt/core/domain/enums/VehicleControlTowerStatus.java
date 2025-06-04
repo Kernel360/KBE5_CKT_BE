1cc05853172f6f35f00e91d26e8d345987cfe652
HAKYUNG SONG <75715353+mannungman@users.noreply.github.com>
2025-06-04 18:54:01 +0900

    refactor: enum 클래스는 통일성을 위해 value 필드와 한글 설명을 포함한 구조로 작성

--- core/src/main/java/kernel360/ckt/core/domain/enums/VehicleControlTowerStatus.java
+++ core/src/main/java/kernel360/ckt/core/domain/enums/VehicleControlTowerStatus.java
@@ -1,7 +1,13 @@
 package kernel360.ckt.core.domain.enums;
 
 public enum VehicleControlTowerStatus {
-    RUNNING,
-    STOPPED,
-    UNTRACKED
+    RUNNING("운행중"),
+    STOPPED("미운행"),
+    UNTRACKED("미관제");
+
+    private final String description;
+
+    VehicleControlTowerStatus(String description) {
+        this.description = description;
+    }
 }
